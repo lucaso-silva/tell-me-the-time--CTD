c1451df3ac10d46ef2ba98e62d0a824f951c4537
lucaso-silva <luc.osilva@gmail.com>
2023-04-19 23:49:53 -0300

    feat:(wip) get current time place function

--- assets/js/getCurrentPlaceTime.js
+++ assets/js/getCurrentPlaceTime.js
@@ -1,20 +1,25 @@
 const myApiKey = config.TZ_API_KEY;
 
 const getCurrentPlaceTime = async (place) => {
-  try {
+
     const response = await fetch(`https://geocode.maps.co/search?q={${place}}`);
     const data = await response.json()
     
     const latitude = data[0].lat;
     const longitude = data[0].lon;
 
-    return latitude, longitude
+    return latitude, longitude;
+    // console.log("Lat: " + latitude, "long: " + longitude);
 
-  } catch (err) {
-    console.log(err);
+}
 
-  }
-};
+//     return latitude, longitude
+
+//   } catch (err) {
+//     console.log(err);
+
+//   }
+// };
 
 const getTimeZone = async(latitude, longitude) => {
     const timeUrl = `https://api.timezonedb.com/v2.1/get-time-zone?key=${myApiKey}&format=json&by=position&lat=${latitude}&lng=${longitude}`
@@ -28,11 +33,30 @@ const getTimeZone = async(latitude, longitude) => {
     return zoneName;
 }
 
-function getCurrentTime(zoneName) {
+const getCurrentTime = (zoneName) => {
     const currentPlaceTime = new Date().toLocaleTimeString("en-US", {timeZone: `${zoneName}`});
-    return currentPlaceTime;
+    // return currentPlaceTime;
+    console.log(currentPlaceTime);
 }
 
-getCurrentPlaceTime("Recife").then((latitude, longitude) => getTimeZone(latitude, longitude))
-                            .then((zoneName) => getCurrentTime(zoneName))
-                            .catch((err) => console.log(err))
+getCurrentPlaceTime("Recife").then((response) => {
+  if(!response.ok) {
+    throw new Error(`HTTP error: ${response.status}`);
+  }
+  return response.json();
+})
+
+.then((latitude, longitude) =>{
+  getTimeZone(latitude,longitude).then((response)=>{
+    if(!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    } else {
+      return response.json();
+    }
+  })
+})
+
+.then((zoneName)=>{
+  getCurrentTime(zoneName);
+
+})
